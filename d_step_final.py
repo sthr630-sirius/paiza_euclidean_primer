ef32b7866d8841f4f93586cef1be55560ac1739c
sthr630-arcturus <s_izumi630y@yahoo.co.jp>
2023-12-16 11:53:07 +0900

    edit d_step_final

--- d_step_final.py
+++ d_step_final.py
@@ -1,8 +1,3 @@
-import math
-def get_pq(n):
-    for i in range(2, int(math.sqrt(n))+1):
-        if n%i == 0:
-            return i
 def extgcd(a, b):
     if b == 0:
         return 1, 0, a
@@ -27,31 +22,58 @@ e --ord--> ord(e)=101 ---bin--> bin(101)=110 0101 ---zfill--> zfill(7)
 s --ord--> ord(s)=115 ---bin--> bin(115)=111 0011 ---zfill--> zfill(7)
 t --ord--> ord(t)=116 ---bin--> bin(116)=111 0100 ---zfill--> zfill(7)
 
-send_message_ori -> send_message_bin -> send_message_dec = M
+send_message -> send_message_bin -> int(send_message_bin, 2) = M
 E = M^e mod n
 
-p = 2
-q = 3
-n = p*q = 6
-n_prime = (p-1) * (q-1) = 2
-e = 9 gcd(e, n_prime) = 1
+p = 23917
+q = 23929
+n = p*q
+n_prime = (p-1) * (q-1)
+gcd(e, n_prime) = 1
 """
 
-n, e, E = map(int, input().split())
-p = get_pq(n)
-q = n//p
+send_message = "tttr"
+send_message_bin = ""
+
+for c in send_message:
+    send_message_bin = send_message_bin + str(bin(ord(c)))[2:].zfill(7)
+
+M = int(send_message_bin.ljust(28, "0"), 2)
+
+print(send_message)
+
+p = 23917
+q = 23929
+n = p*q
 n_prime = (p-1)*(q-1)
+e = 8731
+
+print(f"n: {n}")
+print(f"p: {p}")
+print(f"q: {q}")
+print(f"n': {n_prime}")
+print(f"e: {e}")
+
 d, y, g = extgcd(e, n_prime)
 d = d%n_prime
 
-m = modpow(E, d, n)
-ans_bin = str(bin(m))[2:].zfill(28)
+print(f"gcd(e, n')={g}")
+print(f"d:", d)
+print(f"de= {d*e%n_prime} mod(n')")
+
+E = modpow(M, e, n)
+print(f"M:", M)
+print(f"E:", E)
+decode_M = modpow(E, d, n)
+print(f"deM:", decode_M)
+
+ans_bin = str(bin(decode_M))[2:].zfill(28)
+print(ans_bin)
 ans_str = ""
 
 for i in range(0, 22, 7):
     ans_chr_bin = ans_bin[i:i+7]
     if ans_chr_bin != "0000000":
-        #print(int(str_code_bin, 2))
         ans_str += chr(int(ans_chr_bin, 2))
 
 print(ans_str)
